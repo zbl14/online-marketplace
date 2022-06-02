3be67b2fc9f9b362e390f2e9b280e0d79a316dc3
Sandra Tena <sandratena06@gmail.com>
2022-06-02 11:27:51 -0700

    add code and debug receipt output
    Co-authored-by: Zhibin Liang <ifthereisoneday@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    const personInput = $("input#person").val();
+    const addressInput = $("input#address").val();
+    const food = $("input:radio[pizza]:checked").val();
+    const deliveryDate = $("input#deliveryDate").val();
+    const favoriteColor = $("input#color").val();
+    const receipt = $("output#receipt").val();
+    //$(".person").text(personInput);
+    //$(".address").text(addressInput);
+    //$(".food").text(food);
+    //$(".deliveryDate").text(deliveryDate);
+    //$(".favoriteColor").text(favoriteColor);
+    $(".person").text(personInput);
+    $(".address").text(addressInput);
+    $(".food").text(food);
+    $(".deliveryDate").text(deliveryDate);
+    $(".favoriteColor").text(favoriteColor);
+    $(".receipt").text();
+    $("#story").show();
+  });
+});
